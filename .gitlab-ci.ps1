e77aaee7c36843928d42f945e12c042e42c44bf5
Bert van der Weerd <bert@stanzabird.nl>
2021-03-01 14:21:36 +0100

    setting up for windows_runner

---
+++ .gitlab-ci.ps1
@@ -0,0 +1,7 @@
+$env:MOZILLABUILD = "C:\mozilla-build\"
+$env:USERPROFILE = 'C:\Users\librewolf'
+$env:PATH = "$env:SystemRoot\System32;$env:SystemRoot;$env:SystemRoot\System32\Wbem;"
+
+& ($env:MOZILLABUILD + "msys\bin\bash.exe") -i -c ("/bin/bash.exe build.sh " + $args)
+
+Exit $LASTEXITCODE
